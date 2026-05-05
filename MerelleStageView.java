85f318f66c9df7ce02fb5076de31550180fedf4e
Ecole SAE <ecole@iut-bm.univ-fcomte.fr>
2026-05-05 14:51:46 +0200

    createloook lopp

--- MerelleStageView.java
+++ MerelleStageView.java
@@ -1,4 +1,9 @@
+import boardifier.view.ElementLook;
 import boardifier.view.GameStageView;
+import boardifier.view.GridLook;
+import boardifier.model.ContainerElement;
+import boardifier.model.GameElement;
+import boardifier.model.GameException;
 import boardifier.model.GameStageModel;
 import boardifier.control.StageFactory;
 
@@ -9,6 +14,21 @@ public class MerelleStageView extends GameStageView {
     }
 
     @Override
-    public void createLooks() {
+    public void createLooks() throws GameException {
+        for (GameElement element : gameStageModel.getElements()) {
+            if (element instanceof ContainerElement) {
+                // For the plateau, use a GridLook
+                addLook(new GridLook(3, 3, (ContainerElement) element, 1, 1));
+            } else if (element instanceof Pion) {
+                // For a Pion, create a simple look
+                addLook(new ElementLook(element, 1, 1, 1) {
+                    @Override
+                    public void render() {
+                        Pion p = (Pion) element;
+                        shape[0][0] = (p.getCouleur() == Couleur.BLANC) ? "W" : "B";
+                    }
+                });
+            }
+        }
     }
 }
