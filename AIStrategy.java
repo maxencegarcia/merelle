21b683d60229ac7a2631307eb41e77290d53ff38
Ecole SAE <ecole@iut-bm.univ-fcomte.fr>
2026-06-12 09:50:04 +0200

    mise a jour du main pour qu'il soit a jour a fonctionnel

--- AIStrategy.java
+++
@@ -1,6 +0,0 @@
-
-public interface    AIStrategy {
-    Position choosePlacement(Color myColor, Color enemyColor);
-    Position[] chooseMove(PlayerC player);
-    Position chooseSteal(Color enemyColor);
-}
--- Phase.java
+++
@@ -1,5 +0,0 @@
-public enum Phase {
-    PLACE,
-    MOVE,
-    STEAL
-}
